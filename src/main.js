d5ac4e8588c9a6d193019cca1027e1c2588e3781
javieratorresm <jakane.x@hotmail.com>
2017-10-28 01:25:38 -0300

    uno

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+   data: {
+    message: '',
+  }
+})
+
+Vue.component('buscador', {
+	template: '<div> Un buscador personalizado</div>'
+	})
+
+new Vue ( {
+	el: '#ejemplo',
+
+
+})
+
