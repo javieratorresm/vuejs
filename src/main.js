0b7376eca097beb6cc3a697baf0bde1ed55214c3
JavieraSaezLeon <javiera.saezl@usach.cl>
2017-10-28 21:48:05 -0300

    se arregla estructura y get

--- src/main.js
+++ src/main.js
@@ -1,38 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-var VueResource = require('vue-resource');
-
-Vue.use(VueResource);
-
-
-
-
 new Vue({
   el: '#app',
    data: {
     message: '',
-  }
-})
-
-Vue.component('buscador', {
-	template: '<div> Un buscador personalizado</div>'
-	})
-
-new Vue({
-    el: '#my_view',
-    data: {
-       origin: ''
-    },
-
-    ready: function() {
-
-        // GET request
-this.$http.get('https://swapi.co/api/films', function (data) {            // set data on vm
-            this.$set('origin', data)
-
-        }).error(function (data, status, request) {
-            // handle error
-        })
-
+    origin:''
+  },
+  methods:{
+      getData: function(){
+        this.$http.get('https://swapi.co/api/films').then(response => {
+          this.origin = response.data;
+        }, response => {
+          console.log("ERROR")
+        });
       }
+    },
+   mounted: function(){
+      this.getData();
+   } 
 })
