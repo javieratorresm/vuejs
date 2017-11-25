783ec068e6e3a94285a489206c27ba31bc52b3c9
javieratorresm <jakane.x@hotmail.com>
2017-11-25 19:26:05 -0300

    busqueda con checkbox

--- src/main.js
+++ src/main.js
@@ -3,15 +3,37 @@ new Vue({
 
   data: {
        message: ' ',
+       checked_libro  : false,
+       checked_year  : false,
+       checked_autor  : false,
+       checked_editorial  : false,
        origin:''
         },
 methods:{
 	post: function(){
-    this.$http.post('http://localhost/rest/books/word', {query: this.message}).then (response => {
-  	   console.log(response.data);
+    if(this.checked_libro == true){
+     this.$http.post('http://localhost/rest/books/word', {query: this.message}).then (response => {
+       console.log(response.data);
        this.origin = response.data
-    });
+     })}
+     if (this.checked_autor == true){
+     this.$http.post('http://localhost/rest/books/word', {query: this.message}).then (response => {
+       console.log(response.data);
+       this.origin = response.data
+     })}
+
+if (this.checked_editorial== true){
+     this.$http.post('http://localhost/rest/books/word', {query: this.message}).then (response => {
+       console.log(response.data);
+       this.origin = response.data
+     })}
+     if (this.checked_year == true){
+     this.$http.post('http://localhost/rest/books/word', {query: this.message}).then (response => {
+       console.log(response.data);
+       this.origin = response.data
+     })};
+  
+
+
   }}
 })
-
-
